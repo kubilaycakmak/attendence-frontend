7a2b185c0e9b235361a413da0a4812030988248f
aycadogan89 <doganayca89@gmail.com>
2022-08-18 00:20:18 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from 'react'
 
-import './App.css';
+import './App.css'
+import Calender from '../src/component/Calendar/Calender'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <Calender />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
