c269cbdd2cc08a10057e83b02b3a1c6af3af0b6f
github naotake32 <take32.nao@gmail.com>
2022-08-20 14:48:15 -0700

    The commit for a landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import AppointmentHeader from './component/Appointment-Header/AppointmentHeader';
 
 import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
+      <AppointmentHeader/>
     </div>
+    </>
   );
 }
 
