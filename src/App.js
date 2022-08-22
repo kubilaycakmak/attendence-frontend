ac0c834ed422bf50c773e9db7791d7965482000c
Ryo Somatani <ryo_somatani@outlook.com>
2022-08-22 09:34:35 -0700

    add signup & login page ui

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './component/Footer/Footer';
 import './App.css';
+import LoginSignup from './component/pages/LoginSignup/LoginSignup';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <div className="App"></div>
-        <Footer />
+        <div className="App">
+          <Routes>
+            <Route path="/register" element={<LoginSignup />} />
+            <Route path="/login" element={<LoginSignup />} />
+          </Routes>
+        </div>
+        {/* <Footer /> */}
       </BrowserRouter>
     </>
   );
