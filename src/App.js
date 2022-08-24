8abdd60e3166d2014b8df214a0bb9569fb3f0ef2
Ryo Somatani <ryo_somatani@outlook.com>
2022-08-23 18:58:03 -0700

    add avatar menu component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './component/Footer/Footer';
 import './App.css';
 import LoginSignup from './component/pages/LoginSignup/LoginSignup';
+import AvatarMenu from './component/AvatarMenu/AvatarMenu';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <div className="App">
+          <AvatarMenu />
           <Routes>
             <Route path="/register" element={<LoginSignup />} />
             <Route path="/login" element={<LoginSignup />} />
