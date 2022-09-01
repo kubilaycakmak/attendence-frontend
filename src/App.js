945a7e4d7324fc5f0598a084bfeed76a017bb243
github naotake32 <take32.nao@gmail.com>
2022-09-01 15:59:16 -0700

    The commit for appointment-card component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
-
+import AppointmentCard from "./component/AppointmentCard/AppointmentCard";
+import userDatas from "./component/AppointmentCard/userData.json";
+import "./reset.scss";
 import './App.css';
 
 function App() {
+ 
   return (
+    <>
     <div className="App">
+    {
+     userDatas.map((userData)=>
+      (
+      <AppointmentCard
+        id={userData.id}
+        photo={userData.photo}
+        full_name={userData.full_name}
+        role={userData.role}
+        description={userData.description}
+        />
+        )
+        
+          )
+    }
     </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
-import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
       <App />
-    </Provider>
+    {/* </Provider> */}
   </React.StrictMode>
 );
 
