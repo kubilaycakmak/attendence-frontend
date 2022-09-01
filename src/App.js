fa9112debe1bdec541ca9d91bbb1b5374dbbe5aa
sh32321 <sh32321@gmail.com>
2022-09-01 13:57:06 -0700

    profile-header added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react"
 
-import './App.css';
+// import "./App.css"
+import ProfileHeader from "./component/Profile-header/ProfileHeader"
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <ProfileHeader />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
