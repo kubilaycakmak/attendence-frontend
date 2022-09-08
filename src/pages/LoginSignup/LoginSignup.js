babf070983dbef8e2789185e4d6f4594eb5392e8
Ryo Somatani <ryo_somatani@outlook.com>
2022-09-07 17:38:20 -0700

    add header to login&signup page; adjust header styling

--- src/pages/LoginSignup/LoginSignup.js
+++ src/pages/LoginSignup/LoginSignup.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from '../../component/Header/Header';
 import LoginSignupForm from '../../component/LoginSignupForm/LoginSignupForm';
 import useModal from '../../hooks/useModal';
 import PasswordForm from '../../component/PasswordForm/PasswordForm';
@@ -7,16 +8,19 @@ import styles from './LoginSignup.module.scss';
 const LoginSignup = () => {
   const { Modal, openModal } = useModal();
   return (
-    <div className={styles.login}>
-      <Modal isClosable={false}>
-        <PasswordForm />
-      </Modal>
-      <div className={styles.container}>
-        <div className={styles.loginInner}>
-          <LoginSignupForm googleSignupCallback={openModal} />
+    <>
+      <Header />
+      <div className={styles.login}>
+        <Modal isClosable={false}>
+          <PasswordForm />
+        </Modal>
+        <div className={styles.container}>
+          <div className={styles.loginInner}>
+            <LoginSignupForm googleSignupCallback={openModal} />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
