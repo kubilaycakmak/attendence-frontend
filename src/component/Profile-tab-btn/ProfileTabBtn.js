5a97ac59185c20d423782729cdc297251eb70be4
sh32321 <sh32321@gmail.com>
2022-09-20 14:13:23 -0700

    notifis feature added

--- src/component/Profile-tab-btn/ProfileTabBtn.js
+++ src/component/Profile-tab-btn/ProfileTabBtn.js
@@ -1,68 +1,38 @@
 import React from "react"
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import styles from "./ProfileTabBtn.module.scss"
 
 const ProfileTabBtn = () => {
-  const [isActive, setIsActive] = useState("")
-  const [notifs, setNotifs] = useState(false)
+  const [isActive, setIsActive] = useState(1)
 
-  const data = [{ id: 1, msg: "msg", read: true }]
+  const data = [
+    { id: 1, text: "Information", msg: "msg", read: true },
+    { id: 2, text: "Appointments", msg: "msg", read: true },
+    { id: 3, text: "Reservations", msg: "msg", read: true },
+    { id: 4, text: "Videos", msg: "msg", read: false },
+    ,
+  ]
 
-  const handleClick = (e) => {
-    setIsActive(e.target.id)
+  const handleClick = (id) => {
+    setIsActive(id)
   }
-
-  useEffect(() => {
-    if (data[0].read) {
-      setNotifs(!notifs)
-    }
-  }, [])
 
   return (
     <div className={styles.container}>
-      <div className={styles.notification}>
-        <button
-          id={"1"}
-          key={1}
-          className={isActive === "1" ? styles.active : styles.inActive}
-          onClick={handleClick}>
-          Information
-        </button>
-        {notifs && <div className={styles.badge}>NEW</div>}
-      </div>
-
-      <div className={styles.notification}>
-        <button
-          id={"2"}
-          key={2}
-          className={isActive === "2" ? styles.active : styles.inActive}
-          onClick={handleClick}>
-          Appointments
-        </button>
-        {notifs && <div className={styles.badge}>NEW</div>}{" "}
-      </div>
-
-      <div className={styles.notification}>
-        <button
-          id={"3"}
-          key={3}
-          className={isActive === "3" ? styles.active : styles.inActive}
-          onClick={handleClick}>
-          Reservations
-        </button>
-        {notifs && <div className={styles.badge}>NEW</div>}{" "}
-      </div>
-
-      <div className={styles.notification}>
-        <button
-          id={"4"}
-          key={4}
-          className={isActive === "4" ? styles.active : styles.inActive}
-          onClick={handleClick}>
-          Videos
-        </button>
-        {notifs && <div className={styles.badge}>NEW</div>}{" "}
-      </div>
+      {data.map((item, index) => {
+        return (
+          <div className={styles.notification}>
+            <button
+              id={item.id}
+              key={item.id}
+              className={isActive === item.id ? styles.active : styles.inActive}
+              onClick={() => handleClick(item.id)}>
+              {item.text}
+            </button>
+            {!item.read && <div className={styles.badge}>NEW</div>}
+          </div>
+        )
+      })}
     </div>
   )
 }
