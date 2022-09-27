3ef0f3619ca2a8c6ebbf7a70911892d0aba80a7b
Azar Barfipour <azar.barfipour@gmail.com>
2022-09-27 14:10:22 -0700

    fix conflict

--- src/index.js
+++ src/index.js
@@ -1,24 +1,3 @@
-<<<<<<<<< Temporary merge branch 1
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import App from './App'
-import './index.css'
-
-const container = document.getElementById('root')
-const root = createRoot(container)
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-=========
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
@@ -32,9 +11,8 @@ const root = createRoot(container);
 
 root.render(
   <>
-      <Provider store={store}>
-        <App />
-      </Provider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </>
 );
->>>>>>>>> Temporary merge branch 2
