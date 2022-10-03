53556f5763c25a340c734ae6d1455476afd18168
sh32321 <sh32321@gmail.com>
2022-10-03 14:45:10 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import ProfileTabBtn from "./component/Profile-tab-btn/ProfileTabBtn"
 import Form from "./component/Profile-tab-btn/Form/Form"
+import AppointmentCalendar from "./component/appointment-calendar/AppointmentCalendar"
 
 function App() {
   return (
     <div className='App'>
-      <ProfileTabBtn />
+      {/* <ProfileTabBtn /> */}
+      <AppointmentCalendar />
     </div>
   )
 }
