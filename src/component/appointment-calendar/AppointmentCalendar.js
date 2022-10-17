263646c14d927cd02c0d02500502d3b356e41db0
senathewitch <you@example.com>
2022-10-16 23:05:30 -0700

    calendar-customized-css

--- src/component/appointment-calendar/AppointmentCalendar.js
+++ src/component/appointment-calendar/AppointmentCalendar.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react"
 import Calendar from "react-calendar"
+import './AppointmentCalendar.css'
 // import "react-calendar/dist/Calendar.css"
 // import "./AppointmentCalendar.module.scss"
+
 
 const AppointmentCalendar = () => {
   const [value, onChange] = useState(new Date())
 
   return (
-    <div>
+    <div className="div">
+      <h2 className="h2">Select a Date & Time</h2>
       <Calendar onChange={onChange} value={value} />
     </div>
   )
