0fc0e5b4d76e28bc762a0ab6d56170ebe95a4838
sh32321 <sh32321@gmail.com>
2022-10-27 15:21:11 -0700

    time slot fixed

--- src/component/appointment-calendar/AppointmentBtn.js
+++ src/component/appointment-calendar/AppointmentBtn.js
@@ -6,18 +6,14 @@ const AppointmentBtn = ({ resData, date }) => {
   const [time, setTime] = useState()
   const [year, setYear] = useState(moment().year())
 
-  useEffect(() => {
-    console.log(moment(date).format("DD/MM/YYYY"))
-  }, [date])
-
   const clickHandler = (e) => {
     setTime(e.target.textContent)
   }
 
   const submitHandler = () => {
     const timeStamp = date.getTime()
-    console.log(timeStamp)
-    console.log(time)
+    // console.log(timeStamp)
+    // console.log(time)
   }
 
   return (
@@ -29,11 +25,8 @@ const AppointmentBtn = ({ resData, date }) => {
           ).format("DD/MM/YYYY")
           let convertedDate = moment(date).format("DD/MM/YYYY")
 
-          console.log("find", selectedDate == convertedDate)
           if (selectedDate == convertedDate) {
-            console.log("options", typeof item.options)
             return item.options.map((i, index) => {
-              console.log("i>>>", i.time)
               return (
                 <li key={index} className={styles.li}>
                   <button
