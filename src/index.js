154c8a63e3c7a0d7ee9edbab4445b9255f83b5d7
Ryo Somatani <ryo_somatani@outlook.com>
2022-11-14 14:16:00 -0800

    fix styling for sidebar

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    {/* <Provider store={store}> */}
+    <App />
+    {/* </Provider> */}
   </React.StrictMode>
 );
 
