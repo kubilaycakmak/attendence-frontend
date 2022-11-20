5c15cf053d7a9271f45fbcd75ad07bca29b9ede3
sh32321 <sh32321@gmail.com>
2022-11-20 15:42:20 -0800

    filter func && styling added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import ProfileTabBtn from "./component/Profile-tab-btn/ProfileTabBtn"
 import Form from "./component/Profile-tab-btn/Form/Form"
 import AppointmentCalendar from "./component/appointment-calendar/AppointmentCalendar"
@@ -9,76 +9,101 @@ import AvatarMenu from "./component/AvatarMenu/AvatarMenu"
 import ReservationCardList from "./component/ReservationCardList/ReservationCardList"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Footer from "./component/Footer/Footer"
+import axios from "axios"
 
 function App() {
-  // TODO: temp data
-  // Profile page
-  const list1 = [
-    {
-      image: "/location-sample.jpg",
-      location: "Microsoft Room ~ 4.Flor",
-      date: "Date: Thursday, 23, 2022 11:15am",
-      duration: "Duration: 45 minutes",
-    },
-    {
-      image: "/location-sample.jpg",
-      location: "Microsoft Room ~ 4.Flor",
-      date: "Date: Thursday, 23, 2022 11:15am",
-      duration: "Duration: 45 minutes",
-    },
-    {
-      image: "/location-sample.jpg",
-      location: "Microsoft Room ~ 4.Flor",
-      date: "Date: Thursday, 23, 2022 11:15am",
-      duration: "Duration: 45 minutes",
-    },
-    {
-      image: "/location-sample.jpg",
-      location: "Microsoft Room ~ 4.Flor",
-      date: "Date: Thursday, 23, 2022 11:15am",
-      duration: "Duration: 45 minutes",
-    },
-  ]
-  // Location & Direction page
-  const list2 = [
-    {
-      type: "classroom",
-      image: "/location-sample.jpg",
-      location: "Microsoft Room ~ 4.Flor",
-      description:
-        "This class location is at first floor and next to elevator.",
-    },
-    {
-      type: "classroom",
-      image: "/location-sample.jpg",
-      location: "Microsoft Room ~ 4.Flor",
-      description:
-        "This class location is at first floor and next to elevator. This class location is at first floor and next to elevator. This class location is at first floor and next to elevator.",
-    },
-    {
-      type: "amenity",
-      image: "/location-sample.jpg",
-      location: "Restroom ~ 1st Floor",
-      description:
-        "This class location is at first floor and next to elevator.",
-    },
-    {
-      type: "amenity",
-      image: "/location-sample.jpg",
-      location: "Restroom ~ 1st Floor",
-      description:
-        "This class location is at first floor and next to elevator.",
-    },
-  ]
+  const [roomData, setRoomData] = useState()
+
+  useEffect(() => {
+    const url = "https://attendance-backend-dev.herokuapp.com/api/rooms/"
+    axios
+      .get(url)
+      .then((res) => {
+        // console.log("res", res)
+        setRoomData(res.data)
+      })
+      .catch((error) => console.error(`Error:${error}`))
+  }, [])
+
+  // const roomDetail = [
+  //   {
+  //     _id: "6334eaf064e8a6df4ed2221c",
+  //     name: "Amazon Room",
+  //     type: "classroom",
+  //     floor: 3,
+  //     description: "This room has AC",
+  //     picture_url:
+  //       "http://res.cloudinary.com/ditucvfmr/image/upload/v1664919008/attendance/rooms/6334eaf064e8a6df4ed2221c.png",
+  //     total_seats: 30,
+  //   },
+  //   {
+  //     _id: "6334eaf064e8a6df4ed2221c",
+  //     name: "Amazon Room",
+  //     type: "classroom",
+  //     floor: 3,
+  //     description: "This room has AC",
+  //     picture_url:
+  //       "http://res.cloudinary.com/ditucvfmr/image/upload/v1664919008/attendance/rooms/6334eaf064e8a6df4ed2221c.png",
+  //     total_seats: 30,
+  //   },
+  //   {
+  //     _id: "6335e24c64e8a6df4ed2223f",
+  //     name: "Facebook Room",
+  //     type: "classroom",
+  //     floor: 3,
+  //     description: "This room has a projector",
+  //     picture_url: "https://trtrtewfergregertyuifghj.jpg",
+  //     total_seats: 20,
+  //   },
+  //   {
+  //     _id: "6335f9fc3ae83c2f014f3127",
+  //     name: "Amazon room ~ 1st Floor",
+  //     type: "classroom",
+  //     floor: 1,
+  //     description:
+  //       "This class location is at first floor and next to elevator.",
+  //     picture_url: "",
+  //     total_seats: 30,
+  //     __v: 0,
+  //   },
+  //   {
+  //     _id: "6335f9fc3ae83c2f014f3127",
+  //     name: "Amazon room ~ 1st Floor",
+  //     type: "classroom",
+  //     floor: 1,
+  //     description:
+  //       "This class location is at first floor and next to elevator.",
+  //     picture_url: "",
+  //     total_seats: 30,
+  //     __v: 0,
+  //   },
+  //   {
+  //     _id: "6335fe33ee26feba6c20f4e3",
+  //     name: "Amazon room ~ 1st Floor 10 seats",
+  //     type: "washroom",
+  //     floor: 2,
+  //     description:
+  //       "This class location is at second floor and next to elevator.",
+  //     picture_url: "",
+  //     total_seats: 10,
+  //     __v: 0,
+  //   },
+  // ]
+
   return (
-    <div className='App'>
+    <div>
       {/* <ProfileTabBtn /> */}
       {/* <AppointmentCalendar /> */}
       <BrowserRouter>
-        <div className='App'>
-          <AvatarMenu />
-          <ReservationCardList list={list1} isForReserved={true} />
-          <ReservationCardList list={list2} />
+        <div>
+          {/* <AvatarMenu /> */}
+          {/* <LocationDirectionFilter /> */}
+          {/* <ReservationCardList
+            // list={list1}
+            roomDetail={roomDetail}
+            isForReserved={true}
+          /> */}
+          {roomData && <ReservationCardList roomData={roomData} />}
           <Routes>
             <Route path='/register' element={<LoginSignup />} />
             <Route path='/login' element={<LoginSignup />} />
--- src/component/ReservationCard/ReservationCard.js
+++ src/component/ReservationCard/ReservationCard.js
@@ -1,43 +1,43 @@
-import React from 'react';
-import styles from './ReservationCard.module.scss';
+import React from "react"
+import styles from "./ReservationCard.module.scss"
 
 const ReservationCard = ({ info, isForReserved = false }) => {
-  const bookReservation = () => {};
-  const cancelReservation = () => {};
+  // console.log("info", info)
+  const bookReservation = () => {}
+  const cancelReservation = () => {}
   return (
-    <div className={styles.reservationCard}>
-      <img src={info.image} alt={info.location} />
-      {isForReserved ? (
-        <>
-          <ul>
-            <li className={styles.reservationCard__location}>
-              {info.location}
-            </li>
-            <li className={styles.reservationCard__date}>{info.date}</li>
-            <li className={styles.reservationCard__duration}>
-              {info.duration}
-            </li>
-          </ul>
-          <button onClick={cancelReservation}>Cancel</button>
-        </>
-      ) : (
-        <>
-          <ul>
-            <li className={styles.reservationCard__location}>
-              {info.location}
-            </li>
-            <li className={styles.reservationCard__description}>
-              {info.description}
-            </li>
-          </ul>
-          {/* show button only for classrooms */}
-          {info.type === 'classroom' && (
-            <button onClick={bookReservation}>Reserve</button>
-          )}
-        </>
-      )}
-    </div>
-  );
-};
+    <>
+      <div className={styles.reservationCard}>
+        <img src={info.picture_url} alt={info.location} />
+        {isForReserved ? (
+          <>
+            <ul>
+              <li className={styles.reservationCard__location}>{info.name}</li>
 
-export default ReservationCard;
+              <li className={styles.reservationCard__date}>
+                {info.description}
+              </li>
+            </ul>
+            <button onClick={cancelReservation}>Cancel</button>
+          </>
+        ) : (
+          <>
+            <ul>
+              <li className={styles.reservationCard__location}>{info.name}</li>
+              <li className={styles.reservationCard__description}>
+                {info.description}
+              </li>
+              <p>{info.total_seats}</p>
+            </ul>
+            {/* show button only for classrooms */}
+            {info.type === "classroom" && (
+              <button onClick={bookReservation}>Reserve</button>
+            )}
+          </>
+        )}
+      </div>
+    </>
+  )
+}
+
+export default ReservationCard
